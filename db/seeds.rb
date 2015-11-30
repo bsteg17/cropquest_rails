0502bb1050a1a22d21b3e1cffe3aa93733604001
bsteg17 <bsteg17@gmail.com>
2015-11-30 11:15:29 -0600

    seeded db with equipment types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+EquipmentType.create(name: "Two-wheel tractor", description: "Tractor with one axle, self-powered, self-propelled")
+EquipmentType.create(name: "Cultivator", description: "Softens the soil")
+EquipmentType.create(name: "Seed drill", description: "Plants seeds in soil")
+EquipmentType.create(name: "Rotator", description: "Sprinkler, waters crops")
+EquipmentType.create(name: "Subsoiler", description: "Loosens deep soil")
